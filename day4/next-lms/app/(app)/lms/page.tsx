863a22b01861384525275275defdbd06ccfe58be
Raj Mishra <raj212558@gmail.com>
2026-07-04 17:09:49 +0530

    update it

---
+++ day4/next-lms/app/(app)/lms/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+export default function LMS() {
+  return (
+    <div className="min-h-full flex flex-col"> Hello this is LMS page</div>
+  );
+}
---
+++ day4/next-lms/app/(app)/nextjs/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function Page() {
+  return (
+    <>
+      <div className="max-h-full w-full">Hii This is nextjs page</div>
+    </>
+  );
+}
